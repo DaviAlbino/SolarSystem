4968df203e3a3cf137e87e9d8f846fe963790670
Davi Albino <negroalbino_@hotmail.com>
2022-06-21 20:31:41 -0300

    Trabalhando nos requisitos 3 e 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
@@ -11,8 +12,8 @@ class Title extends React.Component {
   }
 }
 
-// Title.propTypes = {
-//   headline: PropTypes.string.isRequired,
-// };
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
