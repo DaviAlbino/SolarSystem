9b7217e8adb9d31a40db4ec3328f7a4bc9b276b2
Davi Albino <negroalbino_@hotmail.com>
2022-06-22 12:37:13 -0300

    Adicionando MissionCard

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => (<Missions
+          key={ mission.name }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />))}
       </div>
     );
   }
